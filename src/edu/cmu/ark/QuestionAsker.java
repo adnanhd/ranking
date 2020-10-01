467f7b44de9493f3f150ec4a647a71f30888b80d
adnanhd <adnanharundogan@gmail.com>
2020-10-01 17:10:07 +0300

    update QuestionAsker.java 	modified:   .vscode/launch.json 	new file:   .vscode/settings.json 	modified:   src/edu/cmu/ark/QuestionAsker.java

--- src/edu/cmu/ark/QuestionAsker.java
+++ src/edu/cmu/ark/QuestionAsker.java
@@ -70,6 +70,7 @@ public class QuestionAsker {
 		InitialTransformationStep trans = new InitialTransformationStep();
 		QuestionRanker qr = null;
 		
+		
 		qt.setAvoidPronounsAndDemonstratives(false);
 		
 		//pre-load
@@ -163,7 +164,7 @@ public class QuestionAsker {
 				if(doc.length() == 0){
 					break;
 				}
-				
+				GlobalProperties.setDebug(true);
 				long startTime = System.currentTimeMillis();
 				List<String> sentences = AnalysisUtilities.getSentences(doc);
 				
